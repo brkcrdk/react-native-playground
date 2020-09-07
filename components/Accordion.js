a79ef9e52ae4f6e890718791d2f1954c5ab1a6de
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-07 19:08:42 +0300

    Remove unnecessary components from accordion component.

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,7 +1,6 @@
 import React, {useRef, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Text from './Text';
-import RippleButton from './RippleButton';
 import {useTheme} from '../hooks';
 
 const Accordion = () => {
@@ -30,20 +29,6 @@ const Accordion = () => {
   return (
     <View style={s.container}>
       <Text color={currentTheme.text}>Accordion component is here</Text>
-      <RippleButton
-        backgroundColor="transparent"
-        padding={0}
-        marginVertical={10}>
-        <View style={s.accordionItem}>
-          <View style={s.header}>
-            <Text>Test</Text>
-            <Text>Icon</Text>
-          </View>
-          <View>
-            <Text>Content</Text>
-          </View>
-        </View>
-      </RippleButton>
     </View>
   );
 };
