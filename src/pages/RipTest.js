b6c06fd7977885409e8449c410db639a4b323023
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-07 16:16:10 +0300

    Custom ripple component onPress example added.

--- src/pages/RipTest.js
+++ src/pages/RipTest.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
 import {Container, Text, CustomRipple} from '../../components';
+import {useTheme} from '../../hooks';
 
 const RipTest = () => {
+  const {currentTheme} = useTheme();
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => setCount(count + 1);
+  const handleDecrement = () => setCount(count - 1);
+  const handleReset = () => setCount(0);
+
+  const s = StyleSheet.create({
+    container: {
+      width: '100%',
+      justifyContent: 'space-around',
+      flexDirection: 'row',
+    },
+    textContainer: {},
+  });
+
   return (
     <Container>
       <Text color="#fff" fontSize={20} marginBottom={20}>
         RipTest is here
       </Text>
-      <CustomRipple>
-        <View>
-          <Text color="#fff">This is costum ripple button component</Text>
-          <Text color="#fff">This is costum ripple button component</Text>
-          <Text color="#fff">This is costum ripple button component</Text>
-          <Text color="#fff">This is costum ripple button component</Text>
-        </View>
-      </CustomRipple>
+      <View style={s.container}>
+        <CustomRipple backgroundColor={currentTheme.success}>
+          <View>
+            <Text color="#fff">Increment</Text>
+          </View>
+        </CustomRipple>
+        <CustomRipple>
+          <View>
+            <Text color="#fff">Decrement</Text>
+          </View>
+        </CustomRipple>
+        <CustomRipple>
+          <View>
+            <Text color="#fff">Reset</Text>
+          </View>
+        </CustomRipple>
+      </View>
     </Container>
   );
 };
