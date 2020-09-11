c1168dfab934c6fea741f2b738146fbb6efa0aad
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-11 13:32:00 +0300

    Custom accordion item component added.

--- components/Accordion.js
+++ components/Accordion.js
@@ -24,11 +24,33 @@ const Accordion = () => {
       flexDirection: 'row',
       justifyContent: 'space-between',
     },
+    shadowContainer: {
+      width: '100%',
+      marginVertical: 20,
+      backgroundColor: 'white',
+      borderRadius: 15,
+      padding: 20,
+      elevation: 10,
+      shadowColor: '#000',
+      shadowOffset: {width: 0, height: 3},
+      shadowOpacity: 0.5,
+      shadowRadius: 10,
+    },
   });
 
   return (
     <View style={s.container}>
       <Text color={currentTheme.text}>Accordion component is here</Text>
+      <View
+        style={s.shadowContainer}
+        onLayout={(e) => {
+          console.log(e.nativeEvent.layout.height);
+        }}>
+        <Text color="black">Accordion Header</Text>
+        <View>
+          <Text>Container</Text>
+        </View>
+      </View>
     </View>
   );
 };
