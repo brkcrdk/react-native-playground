58a17a22cefe7838f6f03ca2bdd56038bcccbaed
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-18 17:02:47 +0300

    Switch component added.

---
+++ components/Switch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Switch} from 'react-native';
+
+const Switch = () => {
+  return (
+    <View>
+      <Text>Switch is here</Text>
+    </View>
+  );
+};
+
+export default Switch;
