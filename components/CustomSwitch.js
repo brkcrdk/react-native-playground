bc836824cc30ffdc0e57c7b3eaa31e0bb5fec960
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-18 17:20:50 +0300

    Custom switch component updated.

--- components/CustomSwitch.js
+++ components/CustomSwitch.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, Switch} from 'react-native';
 
 const CustomSwitch = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(!isEnabled);
   return (
-    <View>
-      <Text>Switch is here</Text>
-    </View>
+    <Switch
+      trackColor={{false: '#767577', true: '#81b0ff'}}
+      thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+      ios_backgroundColor="#3e3e3e"
+      onValueChange={toggleSwitch}
+      value={isEnabled}
+    />
   );
 };
 
