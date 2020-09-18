90fbfbe43dcb626cea226610e017d7b4c0b66a34
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-18 17:19:00 +0300

    Switch example page theme update.

--- src/pages/Switch.js
+++ src/pages/Switch.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
 import {Container, Text} from '../../components';
+import {useTheme} from '../../hooks';
 
 const Switch = () => {
+  const {currentTheme} = useTheme();
   return (
     <Container>
-      <Text color="#fff" fontSize={20} marginBottom={20}>
+      <Text color={currentTheme.text} fontSize={20} marginBottom={20}>
         Switch Component Example
       </Text>
     </Container>
