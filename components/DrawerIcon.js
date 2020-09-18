389d05016626c354ab7e246b56f7dee4750aa818
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-18 16:42:12 +0300

    Drawer icon updated.

--- components/DrawerIcon.js
+++ components/DrawerIcon.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import {useTheme} from '../hooks';
 
 const DrawerIcon = ({navigation, ...props}) => {
   const {currentTheme} = useTheme();
+  const s = StyleSheet.create({
+    container: {},
+  });
   return (
-    <View marginTop={20} marginLeft={20} flex={0} {...props}>
+    <View>
       <Icon
         name="menu"
         color={currentTheme.primary}
