cccf3a552de5bb5d06d97ba0e3dd0aaaad14e6b4
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-21 13:42:18 +0300

    New colors added.

--- theme.js
+++ theme.js
@@ -9,11 +9,13 @@ const theme = {
   light: {
     background: '#FFF',
     text: '#626262',
+    switchBackground: '#E2E2E2',
     ...extraColors,
   },
   dark: {
     background: '#0F153A',
     text: '#fff',
+    switchBackground: '#262C49',
     ...extraColors,
   },
 };
