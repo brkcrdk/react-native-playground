292add8293d434c51a4d7301518d1fafc592923b
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-09-21 13:43:41 +0300

    Swtich example env updated.

--- src/pages/Switch.js
+++ src/pages/Switch.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import {Container, Text, CustomSwitch} from '../../components';
+import {View, StyleSheet} from 'react-native';
+import {Container, Text, CustomSwitch, NativeSwitch} from '../../components';
 import {useTheme} from '../../hooks';
-
 const Switch = () => {
   const {currentTheme} = useTheme();
+
+  const s = StyleSheet.create({
+    exampleContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      width: '100%',
+    },
+    switch: {
+      alignItems: 'center',
+    },
+  });
+
   return (
     <Container>
       <Text color={currentTheme.text} fontSize={20} marginBottom={20}>
-        Switch Component Example
+        Switch Components Example
       </Text>
-      <CustomSwitch />
+      <View style={s.exampleContainer}>
+        <View style={s.switch}>
+          <Text color={currentTheme.text} fontSize={15} marginBottom={20}>
+            Custom
+          </Text>
+          <CustomSwitch />
+        </View>
+        <View style={s.switch}>
+          <Text color={currentTheme.text} fontSize={15} marginBottom={20}>
+            Native
+          </Text>
+          <NativeSwitch />
+        </View>
+      </View>
     </Container>
   );
 };
