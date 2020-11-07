b0f30db4f4497ee9ddf48f9f8e6792221815c67e
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-11-07 23:11:31 +0300

    Slider events updated.

--- components/CustomSlider.js
+++ components/CustomSlider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Dimensions} from 'react-native';
 import {useSharedValue} from 'react-native-reanimated';
 import {
@@ -7,12 +7,13 @@ import {
 } from 'react-native-gesture-handler';
 const {width} = Dimensions.get('window');
 const SLIDER_HEIGHT = 5;
-const SLIDER_WIDTH = width - 20;
+const SLIDER_WIDTH = width - 50;
 const THUMB_HEIGHT = 20;
 const THUMB_WIDTH = 20;
 
 const CustomSlider = () => {
   const translateX = useSharedValue(0);
+  const [posX, setX] = useState(0);
   const s = StyleSheet.create({
     container: {},
     slider: {
@@ -25,7 +26,8 @@ const CustomSlider = () => {
       position: 'absolute',
       ...StyleSheet.absoluteFillObject,
       top: (SLIDER_HEIGHT - THUMB_HEIGHT) / 2,
-      left: 20,
+      left: posX - THUMB_WIDTH / 2,
+      // left: SLIDER_WIDTH - THUMB_WIDTH / 2,
       height: THUMB_HEIGHT,
       width: THUMB_WIDTH,
       borderRadius: THUMB_HEIGHT / 2,
