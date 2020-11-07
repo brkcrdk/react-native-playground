f267d09f60a729afefc260cf2c720c6d95406d10
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-11-08 00:39:18 +0300

    Shadow component updated.

--- src/pages/Shadow.js
+++ src/pages/Shadow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ScrollView} from 'react-native';
 import {Container, CustomSlider, Text} from '../../components';
 
 const Shadow = () => {
@@ -8,6 +9,27 @@ const Shadow = () => {
         Shadow is here
       </Text>
       <CustomSlider />
+
+      <Text marginTop={20}>
+        TODO: Text componentinin adını güncelle çünkü native text componenti ile
+        karışıyor.TODO: header türleri ekleyip
+      </Text>
+      <Text>
+        TODO: temaya göre yazı boyutu ve kalınlıklarını güncelleyerek bir
+        typography oluşturabilirsin
+      </Text>
+      <Text>
+        TODO: Webde kullandığın gibi bir typography klasörü iyi olur ve richtext
+        gibi bir komponent olur mu test et
+      </Text>
+      <Text>
+        TODO: temaya göre yazı boyutu ve kalınlıklarını güncelleyerek bir
+        typography oluşturabilirsin
+      </Text>
+      <Text>
+        TODO: Webde kullandığın gibi bir typography klasörü iyi olur ve richtext
+        gibi bir komponent olur mu test et
+      </Text>
     </Container>
   );
 };
