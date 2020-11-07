b369eb3edbd20730b941786c606c261faea69dc8
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-11-07 22:53:42 +0300

    Custom slider updated.

--- components/CustomSlider.js
+++ components/CustomSlider.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import {View, Text, StyleSheet, Dimensions} from 'react-native';
-
+import {useSharedValue} from 'react-native-reanimated';
 const {width} = Dimensions.get('window');
 
 const CustomSlider = () => {
+  const SLIDER_HEIGHT = 5;
+  const SLIDER_WIDTH = width - 20;
+  const THUMB_HEIGHT = 20;
+  const THUMB_WIDTH = 20;
   const s = StyleSheet.create({
     container: {},
     slider: {
-      height: 5,
-      width: width - 20,
-      borderRadius: 2.5,
+      height: SLIDER_HEIGHT,
+      width: SLIDER_WIDTH,
+      borderRadius: SLIDER_HEIGHT / 2,
       backgroundColor: 'red',
     },
-    thumb: {},
+    thumb: {
+      position: 'absolute',
+      ...StyleSheet.absoluteFillObject,
+      top: (SLIDER_HEIGHT - THUMB_HEIGHT) / 2,
+      left: 20,
+      height: THUMB_HEIGHT,
+      width: THUMB_WIDTH,
+      borderRadius: THUMB_HEIGHT / 2,
+      backgroundColor: 'black',
+    },
   });
   return (
     <View style={s.container}>
