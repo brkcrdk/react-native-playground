75977c2589dbdbb5656e4d5792ca19d464bb408e
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-11-08 21:28:51 +0300

    Default page context updated.

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import Navigation from './src/Navigation';
 import {useTheme} from './hooks';
+import {DefaultPageContext} from './context/defaultPage';
 
 const App = () => {
   const {currentTheme, appearance} = useTheme();
 
   return (
-    <NavigationContainer>
-      <StatusBar
-        barStyle={`${appearance === 'dark' ? 'light' : 'dark'}-content`}
-        backgroundColor={currentTheme.background}
-      />
-      <Navigation />
-    </NavigationContainer>
+    <DefaultPageContext>
+      <NavigationContainer>
+        <StatusBar
+          barStyle={`${appearance === 'dark' ? 'light' : 'dark'}-content`}
+          backgroundColor={currentTheme.background}
+        />
+        <Navigation />
+      </NavigationContainer>
+    </DefaultPageContext>
   );
 };
 
--- context/defaultPage.js
+++ context/defaultPage.js
@@ -1,3 +1,15 @@
 import React, {useState, createContext} from 'react';
 
-// const DefaultPage
+const DefaultPage = createContext();
+
+const DefaultPageContext = ({children}) => {
+  const [defaultPage, setDefaultPage] = useState('');
+
+  return (
+    <DefaultPage.Provider value={[defaultPage, setDefaultPage]}>
+      {children}
+    </DefaultPage.Provider>
+  );
+};
+
+export {DefaultPage, DefaultPageContext};
