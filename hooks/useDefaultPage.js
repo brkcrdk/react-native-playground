65344eecd83eba182e3a5bb9e0e1dde0f43304b4
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-11-08 22:41:41 +0300

    AsyncStorage added to useDefaultPage hook

--- hooks/useDefaultPage.js
+++ hooks/useDefaultPage.js
@@ -1,8 +1,29 @@
-import {useContext} from 'react';
+import {useContext, useEffect} from 'react';
 import {DefaultPage} from '../context/defaultPage';
+import AsnycStorage from '@react-native-async-storage/async-storage';
+
 const useDefaultPage = () => {
   const [defaultPage, setDefaultPage] = useContext(DefaultPage);
-  return {defaultPage, setDefaultPage};
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await AsnycStorage.getItem('defaultPage');
+        if (value) {
+          console.log(value);
+        }
+      } catch {}
+    };
+  }, []);
+
+  const updateDefaultPage = async (pageName) => {
+    try {
+      await AsnycStorage.setItem('defaultPage', pageName);
+      setDefaultPage(pageName);
+    } catch {}
+  };
+
+  return {defaultPage, setDefaultPage, updateDefaultPage};
 };
 
 export default useDefaultPage;
